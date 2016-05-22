ac9cae4ff9c1a3305badb0250fd98d81f3329efc
javier-troconis-q2 <javier.troconis@q2ebanking.com>
2016-05-22 08:10:34 -0500

    tasks cancellation token

--- src/infra/EventStoreSettings.cs
+++ src/infra/EventStoreSettings.cs
@@ -12,7 +12,5 @@ namespace infra
 		public static readonly UserCredentials Credentials = new UserCredentials("admin", "changeit");
 		public static readonly IPEndPoint HttpEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 2113);
 		public static readonly IPEndPoint TcpEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1113);
-        public static readonly TimeSpan ReadTimeout = TimeSpan.FromSeconds(2);
-        public static readonly TimeSpan WriteTimeout = TimeSpan.FromSeconds(2);
     }
 }
