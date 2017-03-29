e681aca3bcb5565806f2a220cb703811e1dec3d7
javier troconis <jtroconisa@hotmail.com>
2017-03-28 22:27:03 -0500

    register subscriptiongroup/subscriptionprojection

---
+++ src/subscriber/RegisterSubscriptionGroupHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace subscriber
+{
+    public class RegisterSubscriptionGroupHandler
+    {
+    }
+}
