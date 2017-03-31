9dfdb9e8d4d610516180a7f72007f848f625dbb5
javier-troconis-q2 <javier.troconis@q2ebanking.com>
2017-03-31 10:08:45 -0500

    toasyncinput

--- src/shared/FuncExtensions.cs
+++ src/shared/FuncExtensions.cs
@@ -8,41 +8,42 @@ using Microsoft.Extensions.Caching.Memory;
 
 namespace shared
 {
-    public static class FuncExtensions
-    {
-        public static Func<T2> Partial<T1, T2>(this Func<T1, T2> f, T1 a)
-        {
-            return () => f(a);
-        }
+	public static class FuncExtensions
+	{
+		public static Func<T2> Partial<T1, T2>(this Func<T1, T2> f, T1 a)
+		{
+			return () => f(a);
+		}
 
-        public static Func<T2, T3> Partial<T1, T2, T3>(this Func<T1, T2, T3> f, T1 a)
-        {
-            return b => f(a, b);
-        }
+		public static Func<T2, T3> Partial<T1, T2, T3>(this Func<T1, T2, T3> f, T1 a)
+		{
+			return b => f(a, b);
+		}
 
-        public static Func<T2, T3, T4> Partial<T1, T2, T3, T4>(this Func<T1, T2, T3, T4> f, T1 a)
-        {
-            return (b, c) => f(a, b, c);
-        }
-        public static Func<T2, T3, T4, T5> Partial<T1, T2, T3, T4, T5>(this Func<T1, T2, T3, T4, T5> f, T1 a)
-        {
-            return (b, c, d) => f(a, b, c, d);
-        }
+		public static Func<T2, T3, T4> Partial<T1, T2, T3, T4>(this Func<T1, T2, T3, T4> f, T1 a)
+		{
+			return (b, c) => f(a, b, c);
+		}
 
-        public static Func<T2, T3, T4, T5, T6> Partial<T1, T2, T3, T4, T5, T6>(this Func<T1, T2, T3, T4, T5, T6> f, T1 a)
-        {
-            return (b, c, d, e) => f(a, b, c, d, e);
-        }
+		public static Func<T2, T3, T4, T5> Partial<T1, T2, T3, T4, T5>(this Func<T1, T2, T3, T4, T5> f, T1 a)
+		{
+			return (b, c, d) => f(a, b, c, d);
+		}
 
-        public static Func<T1, T3> ComposeForward<T1, T2, T3>(this Func<T1, T2> f1, Func<T2, T3> f2)
-        {
-            return x => f2(f1(x));
-        }
+		public static Func<T2, T3, T4, T5, T6> Partial<T1, T2, T3, T4, T5, T6>(this Func<T1, T2, T3, T4, T5, T6> f, T1 a)
+		{
+			return (b, c, d, e) => f(a, b, c, d, e);
+		}
 
-        public static Func<T1, T3> ComposeBackward<T1, T2, T3>(this Func<T2, T3> f1, Func<T1, T2> f2)
-        {
-            return f2.ComposeForward(f1);
-        }
+		public static Func<T1, T3> ComposeForward<T1, T2, T3>(this Func<T1, T2> f1, Func<T2, T3> f2)
+		{
+			return x => f2(f1(x));
+		}
+
+		public static Func<T1, T3> ComposeBackward<T1, T2, T3>(this Func<T2, T3> f1, Func<T1, T2> f2)
+		{
+			return f2.ComposeForward(f1);
+		}
 
 		public static Func<T1, T2> Memoize<T1, T2>(this Func<T1, T2> f, IMemoryCache cache, MemoryCacheEntryOptions memoryCacheEntryOptions)
 		{
@@ -68,6 +69,11 @@ namespace shared
 		public static Func<Task<T1>, Task<T2>> ToAsyncInput<T1, T2>(this Func<T1, Task<T2>> f)
 		{
 			return async x => await f(await x);
+		}
+
+		public static Func<Task<T1>, Task<T2>> ToAsyncInput<T1, T2>(this Func<T1, T2> f)
+		{
+			return async x => f(await x);
 		}
 	}
 }
