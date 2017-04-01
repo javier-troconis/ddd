11d549ded4de1b76c876d71b5b662cb16d63cd91
javier troconis <jtroconisa@hotmail.com>
2017-04-01 12:15:06 -0500

    anyasync with iteration

--- src/shared/EnumerableExtensions.cs
+++ src/shared/EnumerableExtensions.cs
@@ -9,7 +9,22 @@ namespace shared
     {
 		public static Task<bool> AnyAsync<T>(this IEnumerable<T> seq, Func<T, Task<bool>> predicate)
 		{
-			return seq.Aggregate(Task.FromResult(false), async (x, y) => await x || await predicate(y));
+            return seq.AnyAsync((x, y) => predicate(x));
 		}
+
+        public static Task<bool> AnyAsync<T>(this IEnumerable<T> seq, Func<T, int, Task<bool>> predicate)
+        {
+            return seq.Aggregate(Task.FromResult(new { Passed = false, Iteration = 0 }), 
+                async (x, y) => 
+            {
+                var result = await x;
+                return result.Passed ? result : new { Passed = await predicate(y, result.Iteration), Iteration = result.Iteration + 1 };
+            }, 
+                async x => 
+            {
+                var result = await x;
+                return result.Passed;
+            });
+        }
     }
 }
