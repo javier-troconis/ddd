fd3d1f3ff75d7d4f4106bbd4c442ea59d072d68f
javier-troconis-q2 <javier.troconis@q2ebanking.com>
2017-06-08 14:24:42 -0500

    projs

--- src/command/Program.cs
+++ src/command/Program.cs
@@ -15,31 +15,29 @@ namespace command
     {
         public static void Main(string[] args)
         {
+            var connectionFactory = new EventStoreConnectionFactory(
+                EventStoreSettings.ClusterDns,
+                EventStoreSettings.InternalHttpPort,
+                EventStoreSettings.Username,
+                EventStoreSettings.Password);
+            var connection = connectionFactory.CreateConnection();
+            connection.ConnectAsync().Wait();
+            IEventStore eventStore = new eventstore.EventStore(connection);
+
             while (true)
             {
                 Task.Run(async () =>
                 {
-                    var connectionFactory = new EventStoreConnectionFactory(
-                        EventStoreSettings.ClusterDns,
-                        EventStoreSettings.InternalHttpPort,
-                        EventStoreSettings.Username,
-                        EventStoreSettings.Password);
-                    using (var connection = connectionFactory.CreateConnection())
-                    {
-                        await connection.ConnectAsync();
+                    var streamName = "application-" + Guid.NewGuid().ToString("N").ToLower();
+                    eventStore.WriteEvents(streamName, ExpectedVersion.NoStream, Commands.StartApplicationV2()).Wait();
+                    Console.WriteLine("application started: " + streamName);
 
-                        IEventStore eventStore = new eventstore.EventStore(connection);
+                    var events = await eventStore.ReadEventsForward(streamName);
+                    var state = events.Aggregate(new SubmitApplicationV1State(), MessageHandlerExtensions.Apply);
+                    var newEvents = Commands.SubmitApplicationV1(state, streamName);
+                    await OptimisticEventWriter.WriteEvents(eventStore, streamName, ExpectedVersion.NoStream, newEvents, ConflictResolutionStrategy.IgnoreConflicts);
+                    Console.WriteLine("application submitted: " + streamName);
 
-                        var streamName = "application-" + Guid.NewGuid().ToString("N").ToLower();
-                        eventStore.WriteEvents(streamName, ExpectedVersion.NoStream, Commands.StartApplicationV2()).Wait();
-                        Console.WriteLine("application started: " + streamName);
-
-                        var events = await eventStore.ReadEventsForward(streamName);
-                        var state = events.Aggregate(new SubmitApplicationV1State(), MessageHandlerExtensions.Apply);
-                        var newEvents = Commands.SubmitApplicationV1(state, streamName);
-                        await OptimisticEventWriter.WriteEvents(eventStore, streamName, ExpectedVersion.NoStream, newEvents, ConflictResolutionStrategy.IgnoreConflicts);
-                        Console.WriteLine("application submitted: " + streamName);
-                    }
                     await Task.Delay(1000);
                 }).Wait();
             }
