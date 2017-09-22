fc2bbc71534925e71426ec51097995eea8fbedf7
javier-troconis-q2 <javier.troconis@q2ebanking.com>
2017-09-22 11:34:52 -0500

    generic resolvedeventhandler

--- src/eventstore/MessageHandlerExtensions.cs
+++ src/eventstore/MessageHandlerExtensions.cs
@@ -16,120 +16,60 @@ namespace eventstore
 	public static class MessageHandlerExtensions
 	{
 		//private static readonly Func<Type, ResolvedEvent, object> TryDeserializeEventWithCaching =
-		//	new Func<Type, ResolvedEvent, object>(TryDeserializeEvent)
-		//		.Memoize(
-		//			new MemoryCache(new MemoryCacheOptions()),
-		//			new MemoryCacheEntryOptions()
-		//				.SetSlidingExpiration(TimeSpan.FromSeconds(5)),
-		//			(eventType, resolvedEvent) => eventType == null ? string.Empty : eventType.FullName + resolvedEvent.Event.EventId);
-
+		//		new Func<Type, ResolvedEvent, object>(TryDeserializeEvent)
+		//			.Memoize(
+		//				new MemoryCache(new MemoryCacheOptions()),
+		//				new MemoryCacheEntryOptions()
+		//					.SetSlidingExpiration(TimeSpan.FromSeconds(5)),
+		//				(eventType, resolvedEvent) => eventType == null ? string.Empty : eventType.FullName + resolvedEvent.Event.EventId);
 
 		public static Func<ResolvedEvent, TOut> CreateResolvedEventHandler<TOut>(this IMessageHandler subscriber, TOut unHandledResult)
 		{
-			return new X<TOut>().CreateResolvedEventHandler(subscriber, unHandledResult);
-			//TOut HandleEvent<TRecordedEvent>(TRecordedEvent recordedEvent)
-			//{
-			//	var handler = (IMessageHandler<TRecordedEvent, TOut>)subscriber;
-			//	return handler.Handle(recordedEvent);
-			//}
+			var eventHandlingTypes = subscriber
+				.GetType()
+				.GetMessageHandlerTypes()
+				.Select(x => x.GetGenericArguments()[0].GetGenericArguments()[0])
+				.ToArray();
 
-			//return resolvedEvent =>
-			//{
-			//	var eventHandlingTypes = subscriber
-			//		.GetType()
-			//		.GetMessageHandlerTypes()
-			//		.Select(x => x.GetGenericArguments()[0].GetGenericArguments()[0])
-			//		.ToArray();
-			//	var eventMetadata = JsonConvert.DeserializeObject<Dictionary<string, object>>(Encoding.UTF8.GetString(resolvedEvent.Event.Metadata));
-			//	var topics = ((JArray)eventMetadata[EventHeaderKey.Topics]).ToObject<object[]>();
-			//	var eventType = topics.Join(eventHandlingTypes, x => x, x => x.GetEventStoreName(), (x, y) => y).FirstOrDefault();
-			//	var recordedEvent = TryDeserializeEventWithCaching(eventType, resolvedEvent);
-			//	if (recordedEvent != null)
-			//	{
-			//		return HandleEvent((dynamic)recordedEvent);
-			//	}
-			//	return unHandledResult;
-			//};
+			return resolvedEvent =>
+			{
+				var eventMetadata = JsonConvert.DeserializeObject<Dictionary<string, object>>(Encoding.UTF8.GetString(resolvedEvent.Event.Metadata));
+				var topics = ((JArray)eventMetadata[EventHeaderKey.Topics]).ToObject<object[]>();
+				var eventType = topics.Join(eventHandlingTypes, x => x, x => x.GetEventStoreName(), (x, y) => y).FirstOrDefault();
+				var recordedEvent = TryDeserializeEvent(eventType, resolvedEvent);
+				return recordedEvent != null ? RecordedEventHandler<TOut>.HandleRecordedEvent(subscriber, (dynamic)recordedEvent) : unHandledResult;
+			};
 		}
 
-		//private static object TryDeserializeEvent(Type eventType, ResolvedEvent resolvedEvent)
-		//{
-		//	if (eventType == default(Type))
-		//	{
-		//		return null;
-		//	}
-		//	var recordedEvent = new
-		//	{
-		//		resolvedEvent.OriginalStreamId,
-		//		resolvedEvent.OriginalEventNumber,
-		//		resolvedEvent.Event.EventStreamId,
-		//		resolvedEvent.Event.EventNumber,
-		//		resolvedEvent.Event.EventId,
-		//		resolvedEvent.Event.Created,
-		//		Data = JsonConvert.DeserializeObject(Encoding.UTF8.GetString(resolvedEvent.Event.Data))
-		//	};
-		//	var recordedEventType = typeof(IRecordedEvent<>).MakeGenericType(eventType);
-		//	return Impromptu.CoerceConvert(recordedEvent, recordedEventType);
-		//}
-
-		private class X<TOut>
+		private static object TryDeserializeEvent(Type eventType, ResolvedEvent resolvedEvent)
 		{
-			private static readonly Func<Type, ResolvedEvent, object> TryDeserializeEventWithCaching =
-				new Func<Type, ResolvedEvent, object>(TryDeserializeEvent)
-					.Memoize(
-						new MemoryCache(new MemoryCacheOptions()),
-						new MemoryCacheEntryOptions()
-							.SetSlidingExpiration(TimeSpan.FromSeconds(5)),
-						(eventType, resolvedEvent) => eventType == null ? string.Empty : eventType.FullName + resolvedEvent.Event.EventId);
-
-
-			public Func<ResolvedEvent, TOut> CreateResolvedEventHandler(IMessageHandler subscriber, TOut unHandledResult)
+			if (eventType == default(Type))
 			{
-				var eventHandlingTypes = subscriber
-					.GetType()
-					.GetMessageHandlerTypes()
-					.Select(x => x.GetGenericArguments()[0].GetGenericArguments()[0])
-					.ToArray();
-
-				return resolvedEvent =>
-				{
-					var eventMetadata = JsonConvert.DeserializeObject<Dictionary<string, object>>(Encoding.UTF8.GetString(resolvedEvent.Event.Metadata));
-					var topics = ((JArray)eventMetadata[EventHeaderKey.Topics]).ToObject<object[]>();
-					var eventType = topics.Join(eventHandlingTypes, x => x, x => x.GetEventStoreName(), (x, y) => y).FirstOrDefault();
-					var recordedEvent = TryDeserializeEventWithCaching(eventType, resolvedEvent);
-					if (recordedEvent != null)
-					{
-						return HandleEvent(subscriber, (dynamic)recordedEvent);
-					}
-					return unHandledResult;
-				};
+				return null;
 			}
+			var recordedEvent = new
+			{
+				resolvedEvent.OriginalStreamId,
+				resolvedEvent.OriginalEventNumber,
+				resolvedEvent.Event.EventStreamId,
+				resolvedEvent.Event.EventNumber,
+				resolvedEvent.Event.EventId,
+				resolvedEvent.Event.Created,
+				Data = JsonConvert.DeserializeObject(Encoding.UTF8.GetString(resolvedEvent.Event.Data))
+			};
+			var recordedEventType = typeof(IRecordedEvent<>).MakeGenericType(eventType);
+			return Impromptu.CoerceConvert(recordedEvent, recordedEventType);
+		}
 
-			private static TOut HandleEvent<TRecordedEvent>(IMessageHandler subscriber, TRecordedEvent recordedEvent)
+		private static class RecordedEventHandler<TOut>
+		{
+			public static TOut HandleRecordedEvent<TRecordedEvent>(IMessageHandler subscriber, TRecordedEvent recordedEvent)
 			{
 				var handler = (IMessageHandler<TRecordedEvent, TOut>)subscriber;
 				return handler.Handle(recordedEvent);
 			}
-
-			private static object TryDeserializeEvent(Type eventType, ResolvedEvent resolvedEvent)
-			{
-				if (eventType == default(Type))
-				{
-					return null;
-				}
-				var recordedEvent = new
-				{
-					resolvedEvent.OriginalStreamId,
-					resolvedEvent.OriginalEventNumber,
-					resolvedEvent.Event.EventStreamId,
-					resolvedEvent.Event.EventNumber,
-					resolvedEvent.Event.EventId,
-					resolvedEvent.Event.Created,
-					Data = JsonConvert.DeserializeObject(Encoding.UTF8.GetString(resolvedEvent.Event.Data))
-				};
-				var recordedEventType = typeof(IRecordedEvent<>).MakeGenericType(eventType);
-				return Impromptu.CoerceConvert(recordedEvent, recordedEventType);
-			}
 		}
 	}
+
+	
 }
