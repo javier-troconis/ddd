147d0ad42544f78bfa87ae7df160affc2a3b798b
javier-troconis-q2 <javier.troconis@q2ebanking.com>
2017-10-13 16:26:10 -0500

    fixing msg handler exts

--- src/eventstore/MessageHandlerExtensions.cs
+++ src/eventstore/MessageHandlerExtensions.cs
@@ -15,31 +15,39 @@ namespace eventstore
 {
 	public static class MessageHandlerExtensions
 	{
-		public static Func<ResolvedEvent, Task<ResolvedEvent>> ComposeForward<TSubscriber1, TSubscriber2>(this TSubscriber1 s1, TSubscriber2 s2) where TSubscriber1 : IMessageHandler where TSubscriber2 : IMessageHandler
+		public static Func<ResolvedEvent, Task<ResolvedEvent>> ComposeForward(this IMessageHandler s1, IMessageHandler s2)
 		{
-			var handleResolvedEvent = SubscriberResolvedEventHandleFactory.CreateSubscriberResolvedEventHandle<TSubscriber2, Task>(delegate { return Task.CompletedTask; });
-			return ComposeForward
-				(
-					s1,
-					async resolvedEvent =>
-					{
-						await handleResolvedEvent(s2, resolvedEvent);
-						return resolvedEvent;
-					}
-				);
+			return ComposeForward1((dynamic)s1, (dynamic)s2);
 		}
 
-		public static Func<ResolvedEvent, Task<ResolvedEvent>> ComposeForward<TSubscriber>(this TSubscriber s, Func<ResolvedEvent, Task<ResolvedEvent>> f) where TSubscriber : IMessageHandler
+		public static Func<ResolvedEvent, Task<ResolvedEvent>> ComposeForward(this IMessageHandler s, Func<ResolvedEvent, Task<ResolvedEvent>> f)
+		{
+			return ComposeForward1((dynamic)s, f);
+		}
+
+		private static Func<ResolvedEvent, Task<ResolvedEvent>> ComposeForward1<TSubscriber1, TSubscriber2>(TSubscriber1 s1, TSubscriber2 s2) where TSubscriber1 : IMessageHandler where TSubscriber2 : IMessageHandler
+		{
+			var handleResolvedEvent = SubscriberResolvedEventHandleFactory.CreateSubscriberResolvedEventHandle<TSubscriber2, Task>(delegate { return Task.CompletedTask; });
+			return ComposeForward1
+			(
+				s1,
+				async resolvedEvent =>
+				{
+					await handleResolvedEvent(s2, resolvedEvent);
+					return resolvedEvent;
+				}
+			);
+		}
+
+		private static Func<ResolvedEvent, Task<ResolvedEvent>> ComposeForward1<TSubscriber>(TSubscriber s, Func<ResolvedEvent, Task<ResolvedEvent>> f) where TSubscriber : IMessageHandler
 		{
 			var handleResolvedEvent = SubscriberResolvedEventHandleFactory.CreateSubscriberResolvedEventHandle<TSubscriber, Task>(delegate { return Task.CompletedTask; });
-			return FuncExtensions.ComposeForward(
-				new Func<ResolvedEvent, Task<ResolvedEvent>>(
-					async resolvedEvent =>
-					{
-						await handleResolvedEvent(s, resolvedEvent);
-						return resolvedEvent;
-					}), 
-				f);
+			return new Func<ResolvedEvent, Task<ResolvedEvent>>(
+				async resolvedEvent =>
+				{
+					await handleResolvedEvent(s, resolvedEvent);
+					return resolvedEvent;
+				}).ComposeForward(f);
 		}
 	}
 
