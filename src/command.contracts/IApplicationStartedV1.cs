74bd1a937539cf826460a50c2a62211bb8f4a09c
javier troconis <jtroconisa@hotmail.com>
2018-04-01 13:46:43 -0500

    command reorg

---
+++ src/command.contracts/IApplicationStartedV1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace command.contracts
+{
+    public interface IApplicationStartedV1
+    {
+        string ApplicantSsn { get; }
+    }
+
+}
---
+++ src/command.contracts/IIdentityVerificationCompletedV1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace command.contracts
+{
+    public interface IIdentityVerificationCompletedV1
+    {
+        string TransactionId { get; }
+        string Result { get; }
+    }
+}
