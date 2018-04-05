ddf355b6eabf43dbe029cccc10e9e1ec65067680
javier-troconis-q2 <javier.troconis@q2ebanking.com>
2018-04-05 15:29:47 -0500

    cleaning up

--- src/shared/MessageHandlerExtensions.cs
+++ src/shared/MessageHandlerExtensions.cs
@@ -5,44 +5,44 @@ using System.Threading.Tasks;
 
 namespace shared
 {
-    public static class MessageHandlerExtensions
-    {
-        public static IMessageHandler<Task<T1>, Task<T2>> ToAsyncInput<T1, T2>(this IMessageHandler<T1, Task<T2>> f)
-        {
-            Func<T1, Task<T2>> handle = f.Handle;
-            return new MessageHandler<Task<T1>, Task<T2>>(handle.ToAsyncInput());
-        }
+   // public static class MessageHandlerExtensions
+   // {
+   //     public static IMessageHandler<Task<T1>, Task<T2>> ToAsyncInput<T1, T2>(this IMessageHandler<T1, Task<T2>> f)
+   //     {
+   //         Func<T1, Task<T2>> handle = f.Handle;
+   //         return new MessageHandler<Task<T1>, Task<T2>>(handle.ToAsyncInput());
+   //     }
 
-        public static IMessageHandler<Task<T1>, Task<T2>> ToAsyncInput<T1, T2>(this IMessageHandler<T1, T2> f)
-        {
-            Func<T1, T2> handle = f.Handle;
-            return new MessageHandler<Task<T1>, Task<T2>>(handle.ToAsyncInput());
-        }
+   //     public static IMessageHandler<Task<T1>, Task<T2>> ToAsyncInput<T1, T2>(this IMessageHandler<T1, T2> f)
+   //     {
+   //         Func<T1, T2> handle = f.Handle;
+   //         return new MessageHandler<Task<T1>, Task<T2>>(handle.ToAsyncInput());
+   //     }
 
-        public static IMessageHandler<T1, T3> ComposeForward<T1, T2, T3>(this IMessageHandler<T1, T2> from, IMessageHandler<T2, T3> to)
-        {
-	        Func<T1, T2> handle = from.Handle;
-			return new MessageHandler<T1, T3>(handle.ComposeForward(to.Handle));
-        }
+   //     public static IMessageHandler<T1, T3> ComposeForward<T1, T2, T3>(this IMessageHandler<T1, T2> from, IMessageHandler<T2, T3> to)
+   //     {
+	  //      Func<T1, T2> handle = from.Handle;
+			//return new MessageHandler<T1, T3>(handle.ComposeForward(to.Handle));
+   //     }
 
-        public static IMessageHandler<T1, T3> ComposeBackward<T1, T2, T3>(this IMessageHandler<T2, T3> to, IMessageHandler<T1, T2> from)
-        {
-            return ComposeForward(from, to);
-        }
+   //     public static IMessageHandler<T1, T3> ComposeBackward<T1, T2, T3>(this IMessageHandler<T2, T3> to, IMessageHandler<T1, T2> from)
+   //     {
+   //         return ComposeForward(from, to);
+   //     }
 
-        private class MessageHandler<T1, T2> : IMessageHandler<T1, T2>
-        {
-            private readonly Func<T1, T2> _f;
+   //     private class MessageHandler<T1, T2> : IMessageHandler<T1, T2>
+   //     {
+   //         private readonly Func<T1, T2> _f;
 
-            public MessageHandler(Func<T1, T2> f)
-            {
-                _f = f;
-            }
+   //         public MessageHandler(Func<T1, T2> f)
+   //         {
+   //             _f = f;
+   //         }
 
-            public T2 Handle(T1 message)
-            {
-                return _f(message);
-            }
-        }
-    }
+   //         public T2 Handle(T1 message)
+   //         {
+   //             return _f(message);
+   //         }
+   //     }
+   // }
 }
