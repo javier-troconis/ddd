411142b32142fe0b072ae6ed3101fccc37ea7ea6
javier troconis <jtroconisa@hotmail.com>
2018-04-08 20:14:36 -0500

    type exts

--- src/eventstore/TypeExtensions.cs
+++ src/eventstore/TypeExtensions.cs
@@ -11,24 +11,65 @@ namespace eventstore
         public static Type[] GetMessageHandlerTypes(this Type subscriberType)
         {
             return subscriberType
-				.GetInterfaces()
-				.Where(x => x.IsGenericType && x.GetGenericTypeDefinition() == typeof(IMessageHandler<,>))
-				.ToArray();
+                .GetInterfaces()
+                .Where(x => x.IsGenericType && x.GetGenericTypeDefinition() == typeof(IMessageHandler<,>))
+                .ToArray();
         }
 
         public static string GetStreamName(this Type entityType, Guid identity, string category = "")
         {
             var streamName = $"{(EventStoreObjectName)entityType}_{identity.ToString("N").ToLower()}";
-	        return string.IsNullOrEmpty(category) ? streamName : category + "-" + streamName;
+            return string.IsNullOrEmpty(category) ? streamName : category + "-" + streamName;
         }
 
-		public static string[] GetEventTopics(this Type eventType)
+        public static string[] GetEventTopics(this Type eventType)
         {
-	        return eventType
-		        .GetInterfaces()
-				.Where(x => Attribute.IsDefined(x, typeof(TopicAttribute)))
-		        .Select(x => ((EventStoreObjectName)x).Value)
-				.ToArray();
+            return eventType
+                .GetInterfaces()
+                .Where(x => Attribute.IsDefined(x, typeof(TopicAttribute)))
+                .Select(x => ((EventStoreObjectName)x).Value)
+                .ToArray();
+        }
+
+        public static string GetTopicsProjectionQuery(this Type type)
+        {
+            var messageHandlerTypes = type.GetMessageHandlerTypes();
+            if (!messageHandlerTypes.Any())
+            {
+                return string.Empty;
+            }
+
+            // pull from specific streams ?
+            const string queryTemplate =
+                @"var topics = [{0}];
+
+function handle(s, e) {{
+    var event = e.bodyRaw;
+    if(event !== s.lastEvent) {{ 
+        var message = {{ streamId: '{1}', eventName: '$>', body: event, isJson: false }};
+        eventProcessor.emit(message);
+    }}
+	s.lastEvent = event;
+}}
+
+var handlers = topics.reduce(
+    function(x, y) {{
+        x[y] = handle;
+        return x;
+    }}, 
+	{{
+		$init: function() {{
+			return {{ lastEvent: ''}};
+		}}
+	}});
+
+fromAll()
+    .when(handlers);";
+            var messageHandlingTypes = messageHandlerTypes.Select(x => x.GetGenericArguments()[0].GetGenericArguments()[0]);
+            var topics = messageHandlingTypes.Select(handlingType => ((EventStoreObjectName)handlingType).Value);
+            var query = string.Format(queryTemplate, string.Join(",\n", topics.Select(topic => $"'{topic}'")), ((EventStoreObjectName)type).Value);
+            return query;
+
         }
     }
 }
