b7413f3e8f69a07103ea8a4e5e693b237dc38249
javier troconis <jtroconisa@hotmail.com>
2019-07-21 22:22:44 -0500

    prj

---
+++ src/idology.api.messaging.host/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace idology.api.messaging.host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
