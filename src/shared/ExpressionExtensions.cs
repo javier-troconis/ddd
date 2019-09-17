a0454d5627a64791442faca7e11d65276f2fb34c
javier-troconis-q2 <Javier.Troconis@q2ebanking.com>
2019-09-17 17:02:31 -0500

    valueequalitycomparer

---
+++ src/shared/ExpressionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace shared
+{
+    public static class ExpressionExtensions
+    {
+        public static string GetMemberName<T>(this Expression<Func<T, object>> expression)
+        {
+            string GetMemberName(Expression value)
+            {
+                switch (value)
+                {
+                    case UnaryExpression unaryExpression:
+                        return GetMemberName(unaryExpression.Operand as MemberExpression);
+                    case MemberExpression memberExpression:
+                        return memberExpression.Member.Name;
+                }
+                throw new ArgumentException("Cannot get member name from expression.");
+            }
+
+            return GetMemberName(expression.Body);
+        }
+    }
+}
---
+++ src/shared/ValueEqualityComparer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace shared
+{
+    public class ValueEqualityComparer<T> : IEqualityComparer<T>
+    {
+        private readonly Lazy<string[]> _memberNameExclusions;
+
+        public ValueEqualityComparer(params Expression<Func<T, object>>[] memberExclussions)
+        {
+            _memberNameExclusions = new Lazy<string[]>(() => memberExclussions.Select(ExpressionExtensions.GetMemberName).ToArray());
+        }
+
+        public bool Equals(T x, T y)
+        {
+            var x1 = JObject.Parse(JsonConvert.SerializeObject(x));
+            var y1 = JObject.Parse(JsonConvert.SerializeObject(y));
+            foreach (var memberNameExclusion in _memberNameExclusions.Value)
+            {
+                x1.Remove(memberNameExclusion);
+                y1.Remove(memberNameExclusion);
+            }
+            return JToken.DeepEquals(x1, y1);
+        }
+
+        public int GetHashCode(T obj)
+        {
+            var x = JObject.Parse(JsonConvert.SerializeObject(obj));
+            foreach (var memberNameExclusion in _memberNameExclusions.Value)
+            {
+                x.Remove(memberNameExclusion);
+            }
+            return JsonConvert.SerializeObject(x).GetHashCode();
+        }
+    }
+}
