62ececd4fe7b4b716d237e54be04d4905b3e96a8
javier-troconis-q2 <Javier.Troconis@q2ebanking.com>
2019-09-18 11:13:10 -0500

    xverifyidentityservice

---
+++ src/idology.api.messaging.host/XVerifyIdentityService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using shared;
+
+namespace idology.api.messaging.host
+{
+    public class XVerifyIdentityRequest
+    {
+        public string Ssn { get; set; }
+    }
+
+    public enum XDecision
+    {
+        Passed,
+        Failed
+    }
+
+    public class XVerifyIdentityResponse
+    {
+        public string TransactionId { get; set; }
+        public XDecision Decision { get; set; }
+    }
+
+    public class XVerifyIdentityService : IMessageHandler<XVerifyIdentityRequest, Task<XVerifyIdentityResponse>>
+    {
+        public Task<XVerifyIdentityResponse> Handle(XVerifyIdentityRequest message)
+        {          
+            throw new NotImplementedException();
+        }
+    }
+}
